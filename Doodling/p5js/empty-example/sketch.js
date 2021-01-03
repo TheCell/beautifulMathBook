8670fc5eb3f7cf4523416ab345eed42f9c8d02ce
TheCell <simon_hischier@hotmail.com>
2021-01-03 11:32:57 +0100

    added p5js working folder

---
+++ Doodling/p5js/empty-example/sketch.js
@@ -0,0 +1,11 @@
+// Creating a GUI with options.
+var gui = new dat.GUI({name: 'Customization'});
+var folder1 = gui.addFolder('Setup options');
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
