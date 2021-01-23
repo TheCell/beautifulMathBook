1a202eff15d6c32ac8ed205e3bd4fdbf9098432b
TheCell <simon_hischier@hotmail.com>
2021-01-23 22:02:36 +0100

    canvas can be redrawn

--- Doodling/p5js/Genuary2021/23_3/sketch.js
+++ Doodling/p5js/Genuary2021/23_3/sketch.js
@@ -1,6 +1,13 @@
 let gfx;
-const xSize = 400;
-const ySize = 400;
+const startupParameters = {
+  xSize: 400,
+  ySize: 400,
+  resizeCanvas: function() {
+    createCanvas(startupParameters.xSize, startupParameters.ySize);
+    gfx = createGraphics(startupParameters.xSize, startupParameters.ySize)
+    gfx.background(options.background);
+  }
+}
 const options = {
   background: '#000000',
   c1: '#264653',
@@ -13,13 +20,14 @@ const options = {
   rotationDivider: 16,
   numberOfSquares: 4,
   reducerFactor: 0.9,
+  startOffset: 0,
   autiodraw: false,
   redraw: function () {
     gfx.reset();
     gfx.background(options.background);
     const numberOfSquareCount = options.numberOfSquares * options.numberOfSquares;
-    const xStepWidth = xSize / options.numberOfSquares;
-    const yStepWidth = ySize / options.numberOfSquares;
+    const xStepWidth = startupParameters.xSize / options.numberOfSquares;
+    const yStepWidth = startupParameters.ySize / options.numberOfSquares;
     for (let i = 0; i < numberOfSquareCount; i++) {
       const xMiddle = i % options.numberOfSquares * xStepWidth - xStepWidth / 2 + xStepWidth;
       const yMiddle = parseInt(i / options.numberOfSquares) * yStepWidth - yStepWidth / 2 + yStepWidth;
@@ -33,6 +41,11 @@ const options = {
 
 // Creating a GUI with options.
 var gui = new dat.GUI({name: 'Customization'});
+var startupParameterFolder = gui.addFolder('canvas options');
+gui.remember(startupParameters);
+startupParameterFolder.add(startupParameters, 'xSize', 400);
+startupParameterFolder.add(startupParameters, 'ySize', 400);
+startupParameterFolder.add(startupParameters, 'resizeCanvas');
 var folder1 = gui.addFolder('Setup options');
 gui.remember(options);
 folder1.addColor(options, 'background');
@@ -42,18 +55,19 @@ folder1.addColor(options, 'c3');
 folder1.addColor(options, 'c4');
 folder1.addColor(options, 'c5');
 folder1.add(options, 'squareCountPerCall', 1, 100);
-folder1.add(options, 'startsize', 1, xSize);
+folder1.add(options, 'startsize', 1);
 folder1.add(options, 'rotationDivider', -128, 128).step(0.1);
 folder1.add(options, 'autiodraw', false, true);
 folder1.add(options, 'numberOfSquares', 1, 16).step(1);
-folder1.add(options, 'reducerFactor', 0.01, 1);
+folder1.add(options, 'reducerFactor', 0.01, 2);
+folder1.add(options, 'startOffset', 0, Math.PI * 2);
 folder1.open();
 gui.add(options, 'redraw');
 gui.add(options, 'save');
 
 function setup() {
-  createCanvas(xSize, ySize);
-  gfx = createGraphics(xSize, ySize)
+  createCanvas(startupParameters.xSize, startupParameters.ySize);
+  gfx = createGraphics(startupParameters.xSize, startupParameters.ySize)
   gfx.background(options.background);
   options.redraw();
 }
@@ -71,6 +85,7 @@ function drawSquares(middleX, middleY, startsize, gfx) {
   const colors = [options.c1, options.c2, options.c3, options.c4, options.c5];
   gfx.push();
   gfx.translate(middleX, middleY);
+  gfx.rotate(options.startOffset);
   gfx.noFill();
   for (let i = 0; i < options.squareCountPerCall; i++) {
     gfx.stroke(random(colors));
