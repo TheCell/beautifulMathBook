1940bf71def8cee3e7e30d1a89eca526f1b1f250
Simon Hischier <simon_hischier@hotmail.com>
2025-07-22 22:41:15 +0200

    copied dithering over

--- Doodling/p5js/Dithering/sketch.js
+++ Doodling/p5js/Dithering/sketch.js
@@ -1,28 +1,35 @@
 "use strict";
 
 let gfx, seed;
+let currentImage = null;
 
 const startupParameters = {
   xSize: 600,
-  ySize: 600,
+  ySize: 800,
   resizeCanvas: function() {
     createCanvas(startupParameters.xSize, startupParameters.ySize);
+
     gfx = createGraphics(startupParameters.xSize, startupParameters.ySize)
     gfx.background(options.background);
+    currentImage.resize(startupParameters.xSize, 0);
+    gfx.image(currentImage, 0, 0);
   }
 }
 
 const options = {
-  background: '#212121',
-  foreground: '#ffae23',
+  background: '#000000',
+  factor: 1,
+  useGrayscale: true,
   restart: function () {
     seed = Math.random() * 100000;
     randomSeed(seed);
     gfx.reset();
     gfx.background(options.background);
+    currentImage.resize(startupParameters.xSize, 0);
+    gfx.image(currentImage, 0, 0);
   },
   save: function () {
-    saveCanvas(`Example_${new Date().getFullYear()}_seed-${seed}_date-${Date.now()}`, 'png');
+    saveCanvas(`Dithering_${new Date().getFullYear()}_seed-${seed}_date-${Date.now()}`, 'png');
   },
   loadImage: function() {
     document.getElementById('fileselector').click();
@@ -40,18 +47,29 @@ let folder1 = gui.addFolder('Setup options');
 gui.remember(options);
 folder1.add(options, 'loadImage');
 folder1.addColor(options, 'background');
-folder1.addColor(options, 'foreground');
+folder1.add(options, 'factor').step(1).min(1).max(10);
+folder1.add(options, 'useGrayscale');
 folder1.open();
 gui.add(options, 'restart');
 gui.add(options, 'save');
 
+function preload() {
+  currentImage = loadImage("example_image.jpg");
+}
+
 function setup() {
   startupParameters.resizeCanvas();
   options.restart();
+  noSmooth();
+  currentImage.resize(startupParameters.xSize, 0);
+  gfx.image(currentImage, 0, 0);
 }
 
 function draw() {
-  image(gfx, 0, 0);
+  let fsd = new floydSteinbergDithering(options.factor, options.useGrayscale);
+  const img = fsd.getImage(gfx);
+  // image(gfx, 0, 0);
+  image(img, 0, 0);
 }
 
 function onFileSelected() {
@@ -64,5 +82,8 @@ function onFileSelected() {
 
 function onImageLoaded(image) {
   // now do stuff
-  gfx.image(image, 0, 0);
+  // gfx.image(image, 0, 0);
+  currentImage = image;
+  currentImage.resize(startupParameters.xSize, 0);
+  gfx.image(currentImage, 0, 0);
 }
